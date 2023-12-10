b028929f8bfbc458846f7bab3d9df5898343937c
Sergey Shkaev <shkaev.ss@mail.ru>
2023-12-10 11:40:41 +0300

    В класс Student добавлен пустой конструктор и сетторы

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -7,10 +7,34 @@ public class Student implements Serializable {
     private int age;
     private transient double GPA; // средний бал
 
+    public Student() {
+    }
+
     public Student(String name, int age, double GPA) {
         this.name = name;
         this.age = age;
         this.GPA = GPA;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", GPA=" + GPA +
+                '}';
     }
 
     public String getName() {
